3be310ccf2ca2a14294487833e5686fe984bf65d
Jack Sam <54708993+wacko1805@users.noreply.github.com>
2021-09-21 10:52:50 +1000

    Added AOSP wallpapers

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ header("Content-type: application/json; charset=utf-8");
 $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
 $url = $protocol . $_SERVER['HTTP_HOST'];
 echo "[";
+include('aosp.php');
 include('p6.php');
 include('p5.php');
 include('p4.php');
